1347d1f90aeab77df9dabe5580bc18f86058ab2e
Jasmina904 <aleksanrovnajasmina@gmail.com>
2025-11-01 16:29:14 +0500

    thame:Array

---
+++ array.php
@@ -0,0 +1,25 @@
+<?php
+//Birinchi usul
+$sonlar =array(2,5,23,83,565,2,5,"Test,'Qiymat");
+//Ikkinchi usul
+$sonlar1 = [5,3,56,4,3,6,45,'test',true];
+
+echo $sonlar[4];
+echo "\n";
+ $sonlar[4] =70;
+ echo "\n";
+echo $sonlar[4];
+echo "\n";
+print_r($sonlar);
+//massivga yangi element qo'shish oxiriga 
+array_push($sonlar, 113,45);
+print_r($sonlar);
+echo "\n";
+$car = ["model" => "Cobalt", "brand" => "Chevrolet", "year" => 2025];
+// print_r($car)
+modeecho $car["l"];
+echo "\n";
+
+
+
+?>
---
+++ casting.php
@@ -0,0 +1,33 @@
+<?php
+// Intager
+$a = 5;
+var_dump($a);
+$a = (string) $a;
+var_dump($a);
+
+//Float
+$b = 12.5;
+var_dump($b);
+$b = (string) $b;
+var_dump($b);
+
+//String
+$c = "Hello";
+var_dump ($c);
+$c = (string) $c;
+var_dump($c);
+
+//Boolean
+
+
+
+
+
+
+
+
+
+
+
+
+?>
